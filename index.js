97b89ed7962f9cd43876da22d8a4d708cbc78314
Arky <hitmanarky@gmail.com>
2015-08-05 17:56:38 +0700

    Adding sample code: Log tab URI and modify sample pages

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // Log current tab URL
 
 var self = require("sdk/self");
+var tabs = require("sdk/tabs");
+var pageMod = require("sdk/page-mod");
 
-require("sdk/tabs").on("ready", logURL);
+//var script = require("sdk/self").data.url("index.html");
 
-function logURL(tab) {
-    console.log("The tab url is:" +  tab.url);
-}
 
+
+// Listen for tab content loads.
+tabs.on('ready', function(tab) {
+  console.log('tab is loaded', tab.title, tab.url);
+});
+
+
+pageMod.PageMod({
+  include: "about:newtab, about:blank, about:",
+   contentScript: 'document.body.innerHTML = ' +
+                 ' "<h1>Page matches ruleset</h1>";'
+});
